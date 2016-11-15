d51e4f9e5fee34fe889af026f30ef2f68012574b
mhallin <mhallin@fastmail.com>
2016-11-15 08:48:48 +0100

    Log in user before dropping without UI

--- src/drop/index.ts
+++ src/drop/index.ts
@@ -1,11 +1,20 @@
+import {authenticate} from '../auth';
+
 import {Coordinator, DropCompletionCallback} from './Coordinator';
 import {DropPiece} from './PieceData';
 
 export function drop(piece: DropPiece, callback: DropCompletionCallback) {
-    const coordinator = new Coordinator({
-        piece: piece,
-        onDropCompleted: callback,
-        onInitialCoverImageDidArrive: null,
+    authenticate(successful => {
+        if (successful) {
+            const coordinator = new Coordinator({
+                piece: piece,
+                onDropCompleted: callback,
+                onInitialCoverImageDidArrive: null,
+            });
+            coordinator.commitEditorDefaults();
+        }
+        else {
+            callback(null, new Error('Could not log in'));
+        }
     });
-    coordinator.commitEditorDefaults();
 }
