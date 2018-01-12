3c6c20df7bfed49a818a76c5c2b8e9cae758bc3c
Erik Agsjö <erik.agsjo@allihoopa.com>
2018-01-12 13:49:54 +0100

    Switched to modern package formats, fixed import bug

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'browserify'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
@@ -27,7 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'lib/**/*.js': ['browserify'],
+      'lib/**/*.js': ['webpack'],
     },
 
 
@@ -76,9 +76,7 @@ module.exports = function(config) {
       },
     },
 
-    browserify: {
-      debug: true,
-      transform: [],
+    webpack: {
     },
   });
 }
